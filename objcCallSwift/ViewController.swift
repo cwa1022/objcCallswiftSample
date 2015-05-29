f13c256f0cabaddf4ed5e8bba0fc89d4764aebc7
yuchi-mac <yuchi.tseng@kizipad.com>
2015-05-29 10:08:41 +0800

    init

---
+++ objcCallSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  objcCallSwift
+//
+//  Created by yuchi-mac on 2015/5/29.
+//  Copyright (c) 2015年 yuchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
