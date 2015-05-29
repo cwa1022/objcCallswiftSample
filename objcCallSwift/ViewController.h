0c452c4a398cf9e0bf21992494daa2c821698478
yuchi-mac <yuchi.tseng@kizipad.com>
2015-05-29 10:15:32 +0800

    updata

---
+++ objcCallSwift/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  objcCallSwift
+//
+//  Created by yuchi-mac on 2015/5/29.
+//  Copyright (c) 2015年 yuchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ objcCallSwiftTests/objcCallSwiftTests.m
@@ -0,0 +1,40 @@
+//
+//  objcCallSwiftTests.m
+//  objcCallSwiftTests
+//
+//  Created by yuchi-mac on 2015/5/29.
+//  Copyright (c) 2015年 yuchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface objcCallSwiftTests : XCTestCase
+
+@end
+
+@implementation objcCallSwiftTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
